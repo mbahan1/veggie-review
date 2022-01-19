338bdc7c971f302f86a398014e4f4503136fe34c
bahanm <bahanm@gmail.com>
2022-01-18 20:23:10 -0800

    added review model

---
+++ models/Review.js
@@ -0,0 +1,16 @@
+const mongoose = require("mongoose");
+
+const articleSchema = new mongoose.Schema(
+	{
+		title: { type: String, required: true },
+		body: { type: String, required: true },
+        rating: { type: Number, required: true },
+        //referenced relationship
+        veggie: {type: mongoose.Schema.Types.ObjectId, ref:"Veggie"},
+	},
+	{
+		timestamps: true,
+	}
+);
+
+module.exports = mongoose.model("Review", reviewSchema);
