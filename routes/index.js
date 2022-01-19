609d385154abba9ebb2163cb7d9cfeb0425bf248
kfusilier <kristinfusilier@gmail.com>
2022-01-19 16:56:57 -0700

    OAuth login/logout routes set

--- routes/index.js
+++ routes/index.js
@@ -9,7 +9,22 @@ router.get('/', function (req, res) {
 router.get(
   "/auth/google",
   passport.authenticate("google", { scope: ["profile", "email"] })
-);
+)
+
+// Google callback route
+router.get(
+	'/oauth2callback',
+	passport.authenticate('google', {
+		successRedirect: '/students',
+		failureRedirect: '/',
+	})
+)
+
+// OAuth logout route
+router.get("/logout", function (req, res) {
+    req.logout();
+    res.redirect("/");
+});
 
 module.exports = {
     router,
