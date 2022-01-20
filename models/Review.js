816c3abba068ad511e79b4f35b8d500b163b0324
bahanm <bahanm@gmail.com>
2022-01-19 19:58:07 -0800

    edited reviews model to add referenced patron.

--- models/Review.js
+++ models/Review.js
@@ -2,11 +2,22 @@ const mongoose = require("mongoose");
 
 const reviewSchema = new mongoose.Schema(
 	{
-		title: { type: String, required: true },
-		body: { type: String, required: true },
-        rating: { type: Number, required: true },
+		title: { 
+			type: String,
+			required: [true, "You must provide a review title"],
+		},
+		body: {
+			type: String,
+			required: [true, "You must provide text for your review"],
+		},
+        rating: { 
+			type: Number,
+			required: [true, "You must provide a rating for your review"],
+
+		},
         //referenced relationship
         veggie: {type: mongoose.Schema.Types.ObjectId, ref:"Veggie"},
+		patron: {type: mongoose.Schema.Types.ObjectId, ref:"Patron"},
 	},
 	{
 		timestamps: true,
