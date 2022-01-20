6e28a77cd1365c74a71ffb9d5acaec8e3133d253
mbahan1 <92610825+mbahan1@users.noreply.github.com>
2022-01-19 19:11:41 -0800

    Revert "Solo work kf"

--- routes/index.js
+++ routes/index.js
@@ -1,37 +1,4 @@
-const router = require('express').Router();
-const passport = require('passport');
-
-router.get('/', function (req, res) {
-	res.render('index', {
-        user: req.user,
-    });
-});
-
-// Google OAuth login route
-router.get(
-    "/auth/google",
-    passport.authenticate("google", 
-        { scope: ["profile", "email"] 
-    })
-);
-
-// Google callback route
-router.get(
-	'/oauth2callback',
-	passport.authenticate('google', 
-        {successRedirect: '/students',
-		failureRedirect: '/',
-	})
-);
-
-// OAuth logout route
-router.get("/logout", function (req, res) 
-{req.logout();
- res.redirect("/");
-});
-
 module.exports = {
-router, 
-veggies: require('./veggies'),
-reviews: require('./reviews'),
-};
+	veggies: require('./veggies'),
+	reviews: require('./reviews'),
+}
