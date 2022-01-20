79997fd1bb50ab0f9730360da97f775410c43a44
kfusilier <kristinfusilier@gmail.com>
2022-01-19 17:26:21 -0700

    Add login/logout UI

--- routes/index.js
+++ routes/index.js
@@ -2,32 +2,36 @@ const router = require('express').Router();
 const passport = require('passport');
 
 router.get('/', function (req, res) {
-	res.render('index')
-})
+	res.render('index', {
+        user: req.user,
+    });
+});
 
 // Google OAuth login route
 router.get(
-  "/auth/google",
-  passport.authenticate("google", { scope: ["profile", "email"] })
-)
+    "/auth/google",
+    passport.authenticate("google", 
+        { scope: ["profile", "email"] 
+    })
+);
 
 // Google callback route
 router.get(
 	'/oauth2callback',
-	passport.authenticate('google', {
-		successRedirect: '/students',
+	passport.authenticate('google', 
+        {successRedirect: '/students',
 		failureRedirect: '/',
 	})
-)
+);
 
 // OAuth logout route
-router.get("/logout", function (req, res) {
-    req.logout();
-    res.redirect("/");
+router.get("/logout", function (req, res) 
+{req.logout();
+ res.redirect("/");
 });
 
 module.exports = {
-    router,
-	veggies: require('./veggies'),
-	reviews: require('./reviews'),
-}
+router, 
+veggies: require('./veggies'),
+reviews: require('./reviews'),
+};
