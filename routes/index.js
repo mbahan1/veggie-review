22555ec2125d908ab9bbced50872023b2df7ff3f
kfusilier <kristinfusilier@gmail.com>
2022-01-20 00:19:06 -0700

    fixed user error

--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,7 @@ const router = require('express').Router();
 const passport = require('passport');
 
 router.get('/', function (req, res) {
-	res.render('index', {
+	res.render('home', {
         user: req.user,
     });
 });
